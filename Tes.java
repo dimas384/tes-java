09f1b20e557d64a0eb09e57c84021775b2474f25
dimas384 <dimas_firmansyah_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:14:25 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,8 @@
+public class tes {
+    Run|Debug
+    public static void main (string[] args) {
+        String nama = "Dimas";
+        String kelas ="x RPL 5";
+        System.out.print("halo nama saya"+ nama + "dari kelas"  + kelas);
+    }
+}
